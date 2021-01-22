5db0af087dbd60dd3ac8497008fd38ec7e29e6cb
Md. Sakib Khan <36070242+MdSakibKhan@users.noreply.github.com>
2021-01-22 21:55:49 +0600

    Add files via upload

---
+++ project/View.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body style="background: url(http://www.cooperindustries.com/content/dam/public/Corporate/eaton/United-Spotlight-Banner.jpg); background-size:100% 100%;">
+<h1 align="center"><font size="15">Transport Review System</font></h1><br><br><br><br><br><br>
+<form action="login.php" method = "Post">
+<br><br><br><center><font size="10">Login</font></center><br>
+<center><font size="5">Enter Name:<input type="text" name = "uname"></input><center><br><br>
+<center>Enter Email:<input type="text" name = "umail"></input><center><br><br>
+<center>Enter Number:<input type="text" name = "unumber"><center><br><br>
+<center><input type="submit" name = "submit"></input></font><center>
+</form>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
